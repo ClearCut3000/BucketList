14f12699e1fbe46ed31253a9f02aa14a708bc8f1
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-10-25 22:04:11 +0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Николай Никитин on 25.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
